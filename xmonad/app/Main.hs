8ef3bbfbe3f45fc61762d9c40693789ca758449a
Br3akp01nt <br3akp01nt@protonmail.com>
2024-01-27 20:31:49 +0100

    xmonad fixes

--- xmonad/app/Main.hs
+++ xmonad/app/Main.hs
@@ -2,7 +2,7 @@
 
 module Main where
 
-import XMonad
+import XMonad hiding (restart)
 import qualified XMonad.Layout.Spacing as SPC
 import XMonad.Layout.Accordion
 import XMonad.Layout.Circle
@@ -14,40 +14,53 @@ import XMonad.Util.EZConfig (additionalKeysP, additionalKeys)
 import XMonad.Layout.ComboP (SwapWindow(..))
 import XMonad.Layout.Tabbed
 import Data.Bifunctor (Bifunctor(first))
+import qualified Data.Time.Clock as C
+import XMonad.Br3akp01nt.Programs
 
+
+modmask = mod1Mask
+
+terminalProgram :: String
+terminalProgram = "xfce4-terminal -e tmux"
+
+main :: IO ()
+main = xmonad 
+     $ def 
+     { layoutHook = SPC.spacingWithEdge 15 $ windowNavigation myLayout
+     , modMask = modmask
+     , terminal = terminalProgram
+     , normalBorderColor = "#185c1f"
+     , focusedBorderColor = "#58b8a5"
+     , focusFollowsMouse = False
+     , clickJustFocuses = True
+     , startupHook = "XMonad started" `dzen` 4
+     } `additionalKeys` keybinds
 
 myLayout = 
-    Circle 
-    ||| combineTwo (TwoPane (1 % 10) (1 % 4)) Accordion (Mirror Accordion)
+    combineTwo (TwoPane (1 % 10) (1 % 4)) Accordion (Mirror Accordion)
+    ||| Circle 
     ||| Full 
     ||| Accordion
 
 keybinds :: [((KeyMask, KeySym), X ())]
 keybinds =  
-    [ ((modmask .|. shiftMask, xK_l), spawn "betterlockscreen -l" >> spawn "echo 'locking' | dzen2 -p 4") 
+    [ ((modmask .|. shiftMask, xK_l), lockscreen) 
+    , ((modmask, xK_q), "rebuilding and restarting xmonad" `dzen` 4 >> restart)
     ]
     <>
     map (first (modmask .|. controlMask .|. shiftMask, )) windowSwaps
   where
     windowSwaps = 
-      [ (xK_Left,  sendMessage (Move L))
-      , (xK_Down,  sendMessage (Move D))
-      , (xK_Up,    sendMessage (Move U))
-      , (xK_Right, sendMessage (Move R))
+      [ (xK_Left,  sendMessage $ Move L)
+      , (xK_Down,  sendMessage $ Move D)
+      , (xK_Up,    sendMessage $ Move U)
+      , (xK_Right, sendMessage $ Move R)
       , (xK_s,     sendMessage SwapWindow)
       ]
 
-modmask = mod1Mask
+restart :: X ()
+restart = spawn "if type xmonad; then xmonad --recompile && xmonad --restart; else xmessage xmonad not in \\$PATH: \"$PATH\"; fi"
 
-main :: IO ()
-main = xmonad 
-    $ def 
-    { layoutHook = SPC.spacingWithEdge 15 $ windowNavigation myLayout
-    , modMask = modmask
-    , terminal = "xfce4-terminal"
-    , normalBorderColor = "#185c1f"
-    , focusedBorderColor = "#58b8a5"
-    , focusFollowsMouse = False
-    , clickJustFocuses = True
-    , startupHook = spawn "echo 'XMonad started' | dzen2 -p 4"
-    } `additionalKeys` keybinds
+dzen :: String -> Int -> X ()
+dzen m t = spawn $ "echo '" <> m <> "' | dzen2 -p " <> show t
+
---
+++ xmonad/lib/XMonad/Br3akp01nt/Programs.hs
@@ -0,0 +1,7 @@
+module XMonad.Br3akp01nt.Programs (lockscreen) where
+import XMonad (X)
+import XMonad.Core (spawn)
+
+lockscreen :: X ()
+lockscreen = spawn "betterlockscreen -l"
+
